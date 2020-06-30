d769baf9b946c1b5eb5312ccc728e87a6916899a
vikramvenaktesh <65473880+vikramvenaktesh@users.noreply.github.com>
2020-06-30 16:15:45 -0700

    Add files via upload

---
+++ alphabet.c
@@ -0,0 +1,146 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    char alph;
+    char A;
+    char B;
+    char C;
+    char D;
+    char E;
+    char F;
+    char G;
+    char H;
+    char I;
+    char J;
+    char K;
+    char L;
+    char M;
+    char N;
+    char O;
+    char P;
+    char Q;
+    char R; 
+    char S;
+    char T;
+    char U;
+    char V;
+    char W;
+    char X;
+    char Y;
+    char Z;
+
+    alph = get_char("Enter any letter in the English Alphabet(CAPS)\n");
+    if (alph == 'A')
+    {
+        printf("%c is a vowel\n", alph);
+    }
+    if (alph == 'B')
+    {
+        printf("%c is a consonant\n", alph);
+    }
+     if (alph == 'C')
+    {
+        printf("%c is a consonant\n", alph);
+    }
+     if (alph == 'D')
+    {
+        printf("%c is a consonant\n", alph);
+    }
+    if (alph == 'E')
+    {
+        printf("%c is a vowel\n", alph);
+    }
+     if (alph == 'F')
+    {
+        printf("%c is a consonant\n", alph);
+    }
+     if (alph == 'G')
+    {
+        printf("%c is a consonant\n", alph);
+    }
+     if (alph == 'H')
+    {
+        printf("%c is a consonant\n", alph);
+    }
+    if (alph == 'I')
+    {
+        printf("%c is a vowel\n", alph);
+    }       
+     if (alph == 'J')
+    {
+        printf("%c is a consonant\n", alph);
+    }
+     if (alph == 'K')
+    {
+        printf("%c is a consonant\n", alph);
+    }
+     if (alph == 'L')
+    {
+        printf("%c is a consonant\n", alph);
+    }
+     if (alph == 'M')
+    {
+        printf("%c is a consonant\n", alph);
+    }
+     if (alph == 'N')
+    {
+        printf("%c is a consonant\n", alph);
+    }
+    if (alph == 'O') 
+    {
+        printf("%c is a vowel\n", alph);
+    }
+     if (alph == 'P')
+    {
+        printf("%c is a consonant\n", alph);
+    }
+     if (alph == 'Q')
+    {
+        printf("%c is a consonant\n", alph);
+    }
+     if (alph == 'R')
+    {
+        printf("%c is a consonant\n", alph);
+    }
+     if (alph == 'S')
+    {
+        printf("%c is a consonant\n", alph);
+    }
+     if (alph == 'T')
+    {
+        printf("%c is a consonant\n", alph);
+    }
+    if (alph == 'U')
+    {
+        printf("%c is a vowel\n", alph);
+    }
+     if (alph == 'V')
+    {
+        printf("%c is a consonant\n", alph);
+    }
+     if (alph == 'W')
+    {
+        printf("%c is a consonant\n", alph);
+    }
+         if (alph == 'X')
+    {
+        printf("%c is a consonant\n", alph);
+    }
+    if (alph == 'Y')
+    {
+        printf("%c can be both a vowel or consonant\n", alph);
+    }
+         if (alph == 'Z')
+    {
+        printf("%c is a consonant\n", alph);
+    }
+   
+
+    
+
+
+    
+   
+}
