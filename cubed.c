6f6038cadaf3e939b64dc1456c443a18f7c43610
vikramvenkatesh2003 <65473880+vikramvenkatesh2003@users.noreply.github.com>
2020-07-10 12:45:47 -0700

    Create cubed.c

---
+++ cubed.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    int c;
+    printf("Choose a number:\n");
+    scanf("%d", &n);
+    
+    c = n * n * n;
+    printf("\n Cube of the number is %d \n", 8);
+    
+    return 0;
+}
