94d30d3c7e546afdffa6ff245ce4a82113d4f529
vikramvenkatesh2003 <65473880+vikramvenkatesh2003@users.noreply.github.com>
2020-08-20 20:07:41 -0700

    Create bubblesort.c

---
+++ bubblesort.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	int i,j;
+	
+	//loop for the last index upto which we have to continue comparison
+	for()
+	{
+		//loop for i and i+1 to compare a[i] and a[i+1]
+		for()
+		{
+				if(a[i]>a[i+1])
+				{
+					//exchange a[i] and a[i+1]
+					//use temp to exchange
+				}
+		}
+	}
+}
